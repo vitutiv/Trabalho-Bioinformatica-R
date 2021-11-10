0b4f28d3dd6e8c12dee4779f521699a11f4a823b
joaopedropilastri <joaopedropilastri@gmail.com>
2021-11-10 20:19:26 -0300

    Update Q2-Resolução.R

--- Q2-Resolução.R
+++ Q2-Resolução.R
@@ -4,6 +4,14 @@ matrix_gene_expre <- matrix(genes_expre)
 dados_paciente <- read.delim("MB_pData_rib.txt")
 pacientes_ER_pos=dados_paciente[dados_paciente$ER_IHC=="pos",]
 pacientes_ER_neg=dados_paciente[dados_paciente$ER_IHC=="neg",]
+for (i in 1:1867) {
+  if (pacientes_ER_pos$PATIENT_ID==names(genes_expre)[i]) {
+    c=c+1
+    
+  }
+}
+print(c)
+#genes_ER_pos=genes_expre["MB-0002"==pacientes_ER_pos[1,]$PATIENT_ID]
 
 t.test(genes_expre, dados_paciente)
 
