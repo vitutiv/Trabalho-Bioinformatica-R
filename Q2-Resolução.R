f5523117e9a0e514f717ada9649562a9ce63116b
joaopedropilastri <joaopedropilastri@gmail.com>
2021-11-10 17:42:01 -0300

    Update Q2-Resolução.R

--- Q2-Resolução.R
+++ Q2-Resolução.R
@@ -2,5 +2,9 @@ setwd("C:/Users/jterr/Documents/USP trabalhos/FIgenomica/Trabalho-Bioinformatica
 genes_expre <- read.delim("MB_gene_epression.txt")
 matrix_gene_expre <- matrix(genes_expre)
 dados_paciente <- read.delim("MB_pData_rib.txt")
+pacientes_ER_pos=dados_paciente[dados_paciente$ER_IHC=="pos",]
+pacientes_ER_neg=dados_paciente[dados_paciente$ER_IHC=="neg",]
+
+t.test(genes_expre, dados_paciente)
 
 
