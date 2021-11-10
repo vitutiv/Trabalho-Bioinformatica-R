0db91fbefbe7f770bbdc0121c4c81ace6c4b2265
joaopedropilastri <joaopedropilastri@gmail.com>
2021-11-10 14:36:31 -0300

    q2 começando

---
+++ Q2-Resolução.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/jterr/Documents/USP trabalhos/FIgenomica/Trabalho-Bioinformatica-R")
+genes_expre <- read.delim("MB_gene_epression.txt")
+matrix_gene_expre <- matrix(genes_expre)
+dados_paciente <- read.delim("MB_pData_rib.txt")
+
+
