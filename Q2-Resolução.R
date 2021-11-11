6439fda058842f356256e67c13256f93d1d83a20
joaopedropilastri <joaopedropilastri@gmail.com>
2021-11-11 18:54:15 -0300

    Update Q2-Resolução.R

--- Q2-Resolução.R
+++ Q2-Resolução.R
@@ -1,18 +1,31 @@
 setwd("C:/Users/jterr/Documents/USP trabalhos/FIgenomica/Trabalho-Bioinformatica-R")
+library(stringr)
 genes_expre <- read.delim("MB_gene_epression.txt")
 matrix_gene_expre <- matrix(genes_expre)
 dados_paciente <- read.delim("MB_pData_rib.txt")
 pacientes_ER_pos=dados_paciente[dados_paciente$ER_IHC=="pos",]
 pacientes_ER_neg=dados_paciente[dados_paciente$ER_IHC=="neg",]
+genes_ER_neg<-matrix(448:50)
+genes_ER_pos<-matrix(1447:50)
+
 for (i in 1:1867) {
-  if (pacientes_ER_pos$PATIENT_ID==names(genes_expre)[i]) {
-    c=c+1
-    
+  str_replace_all(dados_paciente$ER_IHC[i], '-', '.')
+  
+}
+
+
+t.test(genes_expre, dados_pacie)
+
+if (pacientes_ER_pos$PATIENT_ID[i]==dados_paciente$PATIENT_ID[i]) {
+  for(j in 1:45)
+  {
+    genes_ER_pos<-matrix_gene_expre[i,j]
   }
 }
-print(c)
-#genes_ER_pos=genes_expre["MB-0002"==pacientes_ER_pos[1,]$PATIENT_ID]
-
-t.test(genes_expre, dados_paciente)
-
-
+else
+{
+  for(j in 1:45)
+  {
+    genes_ER_neg<-matrix_gene_expre[i,j]
+  }
+}
